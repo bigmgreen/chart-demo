b07c9e1ffe1809f704492e5841d59ed5f7bf37d6
张勋 <zhangxun@autohome.com.cn>
2020-04-23 17:19:57 +0800

    feat: update

--- src/index.js
+++ src/index.js
@@ -3,22 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import Apexcharts from './comps/Apexcharts';
 import BizCharts from './comps/BizCharts';
-// import Echarts from './comps/Echarts';
+import Echarts from './comps/Echarts';
 // import EchartsReduce from './comps/EchartsReduce';
 // import G2 from './comps/G2';
 // import Plotly from './comps/Plotly';
-import Deck from './comps/Deck';
+// import Deck from './comps/Deck';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Apexcharts /> */}
-    {/* <BizCharts /> */}
-    {/* <Echarts /> */}
+    <BizCharts />
+    <Echarts />
     {/* <EchartsReduce /> */}
     {/* <G2 /> */}
     {/* <Plotly /> */}
-    <Deck />
+    {/* <Deck /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
